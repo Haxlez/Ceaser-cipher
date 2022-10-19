eb61083812480862fa74af3e4634198f8ac1b200
Haxlez <77619087+Haxlez@users.noreply.github.com>
2022-10-19 13:23:27 +0200

    Add files via upload

---
+++ ceaser2.py
@@ -0,0 +1,42 @@
+import string
+
+alphabets = string.ascii_lowercase + string.ascii_lowercase
+
+sentence = list(input('enter your text: \n').lower())
+
+what_to_do = input(
+    'enter e to ENCRYPT, d to DECRYPT, exit to EXIT the program \n').lower()
+
+shift_number = int(input('enter your shift number from 1 to 25: \n'))
+
+end_program = False
+
+while not end_program:
+    if what_to_do == 'e':
+        for i in range(len(sentence)):
+            if sentence[i] == ' ':
+                sentence[i] = ' '
+            else:
+                new_letter = alphabets.index(sentence[i]) + shift_number
+                sentence[i] = alphabets[new_letter]
+        print(''.join(map(str, sentence)) + " " + str(shift_number))
+        end_program = True
+    elif what_to_do == 'd':
+        for i in range(len(sentence)):
+            if sentence[i] == ' ':
+                sentence[i] = ' '
+            else:
+                new_letter = alphabets.index(sentence[i]) - shift_number
+                sentence[i] = alphabets[new_letter]
+        print(''.join(map(str, sentence)) + " " + str(shift_number))
+        end_program = True
+    else:
+        decide = input(
+            'invalid entry, try again Y for YES, N for NO: \n').lower()
+        if decide == 'y':
+            sentence = list(input('enter your text: \n').lower())
+            what_to_do = input(
+                'enter e to ENCRYPT, d to DECRYPT, exit to EXIT the program \n').lower()
+            shift_number = int(input('enter your shift number from 1 to 25: \n'))
+        else:
+            end_program = True
